981fd9cbd734f845520d944897154f3558ddc03b
Flávio Moreira <Flvio Moreira@DESKTOP-M4C9BEM>
2018-09-15 16:05:48 -0300

    upgrade de codigo

--- DataInfo/src/Estoque.java
+++ DataInfo/src/Estoque.java
@@ -1,14 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Estoque {
 	private char nome;
-	private int unidade;
+	private int unidade;   
 	private int quantidade;
 	private int codigo;
+	private double preco;
+	private static List<Produto> cadastraProduto = new ArrayList<Produto>();
+	
 	public char getNome() {
 		return nome;
 	}
+	public static List<Produto> getCadastraProduto() {
+		return cadastraProduto;
+	}
+	public static void setCadastraProduto(List<Produto> cadastraProduto) {
+		Estoque.cadastraProduto = cadastraProduto;
+	}
 	public void setNome(char nome) {
 		this.nome = nome;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
 	}
 	public int getUnidade() {
 		return unidade;
--- DataInfo/src/Produto.java
+++ DataInfo/src/Produto.java
@@ -1,4 +1,56 @@
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class Produto extends Estoque{
-
+public class Produto extends JFrame{
+	private List<Produto> cadastraProduto = new ArrayList<Produto>();
+	private JLabel lblProduto, lblCodigo, lblQuantidade, lblPreco, lblDescricao, lblVazio;
+	private JTextField txtProduto, txtCodigo, txtQuantidade, txtPreco, txtDescricao;
+	private JButton btnSalvar;
+	private JPanel painel;
+	
+	public Produto(String titulo) {
+		super(titulo);
+		lblProduto = new JLabel("Produto: ");
+		lblCodigo = new JLabel("Código: ");
+		lblQuantidade = new JLabel("Quantidade: ");
+		lblPreco = new JLabel("Preço: ");
+		lblDescricao = new JLabel("Descrição: ");
+		lblVazio = new JLabel(" ");
+		txtProduto = new JTextField(40);
+		txtCodigo = new JTextField(20);
+		txtQuantidade = new JTextField(40);
+		txtPreco = new JTextField(20);
+		txtDescricao = new JTextField(40);
+		
+		btnSalvar = new JButton("Salvar cadastro");
+		
+		painel = new JPanel();
+		painel.setLayout(new GridLayout(6,2));
+		painel.add(lblProduto);
+		painel.add(txtProduto);
+		painel.add(lblCodigo);
+		painel.add(txtCodigo);
+		painel.add(lblDescricao);
+		painel.add(txtDescricao);
+		painel.add(lblQuantidade);
+		painel.add(txtQuantidade);
+		painel.add(lblPreco);
+		painel.add(txtPreco);
+		painel.add(lblVazio  );
+		
+		painel.add(btnSalvar);
+		
+		this.getContentPane().add(painel);;
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.pack();
+		this.setLocation(100, 100);
+		this.setVisible(true);
+	}
+	
 }
